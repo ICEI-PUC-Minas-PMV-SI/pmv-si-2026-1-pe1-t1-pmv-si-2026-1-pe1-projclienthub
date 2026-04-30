b68b0d5f9e33605a4a5e71357cfab4996fca6dea
DavisLobo <davisrlobos@gmail.com>
2026-04-30 19:31:12 -0300

    new branch update

---
+++ src/js/userPermissions.js
@@ -0,0 +1,23 @@
+// Listar Usuarios
+
+
+
+
+// Cadastrar Usuarios
+
+
+
+
+// Editar usuarios
+
+
+
+
+// Definir perfis de acesso e bloquear areas nao autorizadas
+// 1- Admin, 2- Gerente, 3- Analista e 4- Operador
+
+
+
+
+// Restringir exibicao de dados sensiveis conforme perfil
+
